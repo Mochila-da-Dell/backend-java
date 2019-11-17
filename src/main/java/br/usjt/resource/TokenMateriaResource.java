18a6cc66838249d8e3bc0266f976e66d121fcffe
Vinnilmg <vini.delimagomes@gmail.com>
2019-11-17 18:42:08 -0300

    cors fix

--- src/main/java/br/usjt/resource/TokenMateriaResource.java
+++ src/main/java/br/usjt/resource/TokenMateriaResource.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import br.usjt.repository.TokenMateriaRepository;
 import br.usjt.service.MateriaService;
 import br.usjt.service.TokenMateriaService;
 
+@CrossOrigin 
 @RestController
 @RequestMapping("/token-materia")
 public class TokenMateriaResource {
